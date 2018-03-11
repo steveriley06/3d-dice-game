6273d708f229cd501140eeeb4c8712e0458f2c2d
seis-602-fall2017 <darius.nyaundi@LAPTOP-QG5H3RPR>
2018-03-11 13:44:55 -0500

    Made a if and else line of code.

--- dice game/src/dice/dice.java
+++ dice game/src/dice/dice.java
@@ -3,6 +3,11 @@ package dice;
 public class dice {
 	public static void main( String[] args ) {
 	int steve = 12;
-			if (steve == 1)
+			if (steve == 12)
 	System.out.println("hi");
+			
+
+else 
+		//wrote a line of else code
+		System.out.println("number is not 1");
 }}
