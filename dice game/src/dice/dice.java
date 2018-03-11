1151a9b00090c394a703e45db5b1e03034ad9c73
seis-602-fall2017 <darius.nyaundi@LAPTOP-QG5H3RPR>
2018-03-11 13:29:31 -0500

    Make a if line of code.

--- dice game/src/dice/dice.java
+++ dice game/src/dice/dice.java
@@ -2,6 +2,7 @@ package dice;
 
 public class dice {
 	public static void main( String[] args ) {
-	
-	System.out.println("steve was writing code");
+	int steve = 12;
+			if (steve == 1)
+	System.out.println("hi");
 }}
